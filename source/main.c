58f2cbed45ba4f405685a6e3e64735889177ffe7
Jesse Cazun <jcazu001@wch136-22.cs.ucr.edu>
2021-10-03 17:20:57 -0700

    Completed Part 1

--- source/main.c
+++ source/main.c
@@ -1,23 +1,38 @@
 /*	Author: jcazu001
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab 2  Exercise 1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
+
 #include <avr/io.h>
 #ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
+#include <simAVRHeader.h>
+#endif	
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
-
-    /* Insert your solution below */
-    while (1) {
-
-    }
-    return 1;
+	DDRA = 0x00;// PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0xFF;// PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+//	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+//	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+while(1) {
+if ((PINA1 == 0) && (PINA0 == 1))
+{
+PINB0  = 1; 
 }
+else
+{
+PINB 0 = 0;
+}
+
+}
+
+
+														 												 					return 0;
+		 																	 					
+}
+				 											
+
