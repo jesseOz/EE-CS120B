4f5be13a0840fd5d1be95d50105ece1e10f138ed
Jesse Cazun <jcazu001@wch136-22.cs.ucr.edu>
2021-10-03 23:22:20 -0700

    Completed part 3

---
+++ turnin/[cslogin]_lab2_part1.c
@@ -0,0 +1,32 @@
+/*	Author: jcazu001
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include <simAVRHeader.h>
+#endif	
+
+int main(void) {
+	DDRA = 0x00; // PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0xFF; // PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+//	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+//:wnsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+	while(1) {
+	if ((PORTA == 0x01))
+	{
+	PORTB  = 0x01; 
+	}
+	else
+	{
+	PORTB = 0x00;
+	}
+	}
+
+	return 0;
+	}														 												 					 						 																	 					
---
+++ turnin/[cslogin]_lab2_part3.c
@@ -0,0 +1,46 @@
+/*	Author: jcazu001
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include <simAVRHeader.h>
+#endif	
+
+int main(void) {
+	DDRA = 0x00;// PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRC = 0xFF;
+//	unsigned chcr tmpB = 0x00; // Temporary variable to hold the value of B
+	//	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+	while(1) {
+
+	if (PINA == 0x00)
+	{
+	PINC = 0x04;	}
+
+	else if (PINA == 0x01 || PINA == 0x02 || PINA == 0x03 || PINA == 0x04)
+	{
+	PINC = 0x03; 
+	}
+
+	else if (PINA == 0x03 || PINA == 0x05 || PINA == 0x09 || PINA == 0x0A || PINA == 0x06 || PINA == 0x0C)
+	{
+	PINC = 0x02;
+	}
+
+	else if (PINA == 0x07 || PINA == 0x0E || PINA == 0x0B || PINA == 0x0D)
+	{
+	PINC = 0x01;
+	}
+
+	else 
+	{
+	PINC = 0x80;
+	}
+	}
+		return 1;													 												 				}														 												 					 						 																	 					
