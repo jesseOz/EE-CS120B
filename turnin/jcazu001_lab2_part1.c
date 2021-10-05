bc23ba4b762e84d70399b2569952695e3837b4fb
Jesse Cazun <jcazu001@wch136-22.cs.ucr.edu>
2021-10-05 15:18:21 -0700

    done

--- turnin/jcazu001_lab2_part1.c
+++ turnin/jcazu001_lab2_part1.c
@@ -16,10 +16,9 @@ int main(void) {
 	DDRA = 0x00;  PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRB = 0xFF;  PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
 //	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
-//	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
-	while(1) {
-	
-	if ((PINA & 0x01 == 0x01))
+	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+	while(1) {		
+	if ((PINA & 0x01) == 0x01)
 	{
 	PORTB  = 0x01; 
 	}
